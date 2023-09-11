9eeac7c41d35c2369ec835dc8d8b3122334765ab
Brokeice123 <keithmarkomen@gmail.com>
2023-09-11 14:58:10 +0300

    Project

---
+++ shell.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * splitArguments - function that handles arguments
+ * @input: takes input of user
+ * @args: array that holds arguments.
+ */
+
+void splitArguments(char *input, char *args[])
+{
+	int arg_count = 0;
+
+	args[arg_count] = strtok(input, " ");
+	while (args[arg_count] != NULL)
+	{
+		arg_count++;
+		args[arg_count] = strtok(NULL, " ");
+	}
+
+	args[arg_count] = NULL;
+}
+
+/**
+ *main - a UNIX command line interpreter
+ *@argc: The number of command-line arguments
+ *@argv: An array of strings representing the command-line arguments
+ *
+ *Return: 0 when successful or 1 when otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	size_t input_len = 0;
+	char *input = NULL;
+
+	(void)argv;
+
+	if (isatty(STDIN_FILENO) && argc == 1)
+	{
+		while (1)
+		{
+			char prompt[] = "simple_shell$ ";
+
+			write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
+
+			getline(&input, &input_len, stdin);
+			input[_strlen(input) - 1] = '\0';
+
+			if (_strcmp(input, "exit") == 0)
+				break;
+
+			handleCommand(input);
+		}
+	}
+	else
+	{
+		while (getline(&input, &input_len, stdin) != -1)
+		{
+			input[_strlen(input) - 1] = '\0';
+			handleCommand(input);
+		}
+	}
+	free(input);
+	return (0);
+}
